122e81ff428f600e62f13def6c669a72ebf33a96
Leonardo <leonardo.fg95@gmail.com>
2022-04-20 22:14:20 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Inicio script navbar 
+
+
+// Ativar botão menu
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+
+// Mostrar e ocultar submenus
+$('.minha-biblioteca').click(function () {
+    $('.itens-minha-biblioteca').toggleClass('mostra')
+})
+
+$('.leituras').click(function () {
+    $('.itens-leituras').toggleClass('mostra')
+})
+
+// Girar seta do menu
+$('.minha-biblioteca').click(function () {
+    $('.menu li a .seta-minha-biblioteca').toggleClass('girar')
+})
+
+$('.leituras').click(function () {
+    $('.menu li a .seta-leituras').toggleClass('girar')
+})
